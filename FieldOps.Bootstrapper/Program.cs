fbe32909045b9aac941969c0b2cda9ea757369b2
BitKitsu <nwknt@tutanota.de>
2026-08-26 16:37:01 +0200

    feat(work-orders): integrate module an tests

--- FieldOps.Bootstrapper/Program.cs
+++ FieldOps.Bootstrapper/Program.cs
@@ -1,6 +1,7 @@
 using FieldOps.Modules.Accounts.Api;
 using FieldOps.Modules.Operators.Api;
 using FieldOps.Modules.Technicians.Api;
+using FieldOps.Modules.WorkOrders.Api;
 using FieldOps.Shared.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddInfrastructure();
 builder.Services.AddAccountsModule();
 builder.Services.AddOperatorsModule();
 builder.Services.AddTechniciansModule();
+builder.Services.AddWorkOrdersModule();
 
 
 var app = builder.Build();
@@ -17,4 +19,5 @@ app.UseInfrastructure();
 app.UseAccountsModule();
 app.UseOperatorsModule();
 app.UseTechniciansModule();
+app.UseWorkOrdersModule();
 app.Run();
