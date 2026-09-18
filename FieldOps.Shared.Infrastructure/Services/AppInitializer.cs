991d8784a6a1918e9385209868a7c5eff221377e
BitKitsu <nwknt@tutanota.de>
2026-09-18 14:18:33 +0200

    fix: handle exception in migration scan

--- FieldOps.Shared.Infrastructure/Services/AppInitializer.cs
+++ FieldOps.Shared.Infrastructure/Services/AppInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,16 +14,34 @@ internal class AppInitializer(IServiceProvider serviceProvider, ILogger<AppIniti
     public async Task StartAsync(CancellationToken ct)
     {
         var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(a => a.GetTypes())
-            .Where(t => typeof(DbContext).IsAssignableFrom(t) && typeof(DbContext) != t && t.IsInterface == false);
+            .SelectMany(a =>
+            {
+                try { return a.GetTypes(); }
+                catch (ReflectionTypeLoadException e) { return e.Types!; }
+            })
+            .Where(t => t is not null && typeof(DbContext).IsAssignableFrom(t) && typeof(DbContext) != t && !t.IsInterface);
 
         using var scope = serviceProvider.CreateScope();
 
         foreach (var dbContextType in dbContextTypes)
         {
-            if (scope.ServiceProvider.GetService(dbContextType) is not DbContext dbContext) continue;
+            logger.LogInformation("Migrating {DbContext}", dbContextType.Name);
 
-            await dbContext.Database.MigrateAsync(ct);
+            if (scope.ServiceProvider.GetService(dbContextType) is not DbContext dbContext)
+            {
+                logger.LogWarning("DbContext {DbContext} not found in DI container", dbContextType.Name);
+                continue;
+            }
+
+            try
+            {
+                await dbContext.Database.MigrateAsync(ct);
+                logger.LogInformation("Successfully migrated {DbContext}", dbContextType.Name);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to migrate {DbContext}", dbContextType.Name);
+            }
         }
     }
 
